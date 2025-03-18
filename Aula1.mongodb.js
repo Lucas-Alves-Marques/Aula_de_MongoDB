9a3f30c16817a66e05166801ca91ccc35f62aa72
Lucas Alves Marques <160551088+Lucas-Alves-Marques@users.noreply.github.com>
2025-03-18 20:37:08 -0300

    Aula 3: CRUD no MongoDB

--- Aula1.mongodb.js
+++ Aula1.mongodb.js
@@ -2,12 +2,15 @@
 const database = 'BD3-Aula';
 
 // constante que define nome da coleção do banco de dados
-const collection = 'Livraria';
+// const collection = 'Livraria';
 
 // Define o banco de dados que será utilizado
 use(database);
 
 // Define/cria a coleção de dados que será utilizada
-db.createCollection(collection)
+// db.createCollection(collection)
+db['Livraria'].find();
+
+
 
 
